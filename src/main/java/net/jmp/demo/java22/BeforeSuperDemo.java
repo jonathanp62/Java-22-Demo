323e97e704fcbb2bb345667b2c3d15348153bc7b
Jonathan Parker <jonathanp62@gmail.com>
2024-08-08 07:07:47 -0400

    Completed statements before super

--- src/main/java/net/jmp/demo/java22/BeforeSuperDemo.java
+++ src/main/java/net/jmp/demo/java22/BeforeSuperDemo.java
@@ -30,12 +30,20 @@ package net.jmp.demo.java22;
  * SOFTWARE.
  */
 
+import java.util.Objects;
+
 import org.slf4j.LoggerFactory;
 
 import org.slf4j.ext.XLogger;
 
 /**
  * A class the demonstrates statements before 'super()' in a constructor.
+ * This allows developers to place essential initialization logic before
+ * invoking the superclass constructor. Additionally, this can be used to
+ * transform values received in the derived class before calling the base
+ * class constructor. Note that statements we put before super() cannot
+ * access instance variables or execute methods and access to "this" is
+ * prohibited.
  */
 final class BeforeSuperDemo implements Demo {
     /** The logger. */
@@ -55,6 +63,73 @@ final class BeforeSuperDemo implements Demo {
     public void demo() {
         this.logger.entry();
 
+        final var square = new Square(4, 3, "Yellow");
+
+        this.logger.info(square.toString());
+
+        try {
+            new Square(4, 0, "Orange");
+        } catch (final IllegalArgumentException iae) {
+            this.logger.error(iae.getMessage());
+        }
+
         this.logger.exit();
+    }
+
+    /**
+     * A shape.
+     */
+    static class Shape {
+        protected String color;
+
+        /**
+         * The constructor.
+         *
+         * @param   color   java.lang.String
+         */
+        public Shape(final String color) {
+            super();
+
+            this.color = Objects.requireNonNull(color);
+        }
+    }
+
+    /**
+     * A square.
+     */
+    static final class Square extends Shape {
+        private final int sides;
+        private final int length;
+
+        /**
+         * The constructor.
+         *
+         * @param   sides   int
+         * @param   length  int
+         */
+        Square(final int sides, final int length, final String color) {
+            if (sides != 4) {
+                throw new IllegalArgumentException("Squares must have four sides");
+            }
+
+            if (length <= 0) {
+                throw new IllegalArgumentException("Length must be greater than 0");
+            }
+
+            super(color);
+
+            this.sides = sides;
+            this.length = length;
+        }
+
+        /**
+         * The to-string method.
+         *
+         * @return  java.lang.String
+         */
+        @Override
+        public String toString() {
+            return STR."Square {sides=\{this.sides}, length=\{this.length}, color=\"\{this.color}\"";
+        }
     }
 }
