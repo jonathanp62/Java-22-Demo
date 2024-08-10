bcb3132a7c493b15d74464510b45d3c292ba1ad5
Jonathan Parker <jonathanp62@gmail.com>
2024-08-10 08:16:17 -0400

    Began work on the first-last gatherer

---
+++ src/main/java/net/jmp/demo/java22/gatherers/FindLastGatherer.java
@@ -0,0 +1,60 @@
+package net.jmp.demo.java22.gatherers;
+
+/*
+ * (#)FindLastGatherer.java 0.4.0   08/10/2024
+ *
+ * @author   Jonathan Parker
+ * @version  0.4.0
+ * @since    0.4.0
+ *
+ * MIT License
+ *
+ * Copyright (c) 2024 Jonathan M. Parker
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import java.util.function.Predicate;
+
+import java.util.stream.Gatherer;
+
+/**
+ * This gatherer filters out items based on a predicate function and returns the last.
+ * The optional combiner operation is not present in this gatherer.
+ *
+ * @param   <T> The type of input elements to the gathering operation
+ * @param   <A> The potentially mutable state type of the gathering operation
+ */
+public final class FindLastGatherer<T, A> {
+    /** The predicate function. */
+    private final Predicate<T> predicate;
+
+    /**
+     * The constructor.
+     *
+     * @param   predicate   java.util.function.Predicate&lt;T&gt;
+     */
+    public FindLastGatherer(final Predicate<T> predicate) {
+        this.predicate = Objects.requireNonNull(predicate);
+    }
+}
