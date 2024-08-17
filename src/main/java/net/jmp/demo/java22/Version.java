73582815a23e999e0943b8d4fd880aaf22bbd001
Jonathan Parker <jonathanp62@gmail.com>
2024-08-17 16:58:56 -0400

    Upgraded the version to 0.7.0

--- src/main/java/net/jmp/demo/java22/Version.java
+++ src/main/java/net/jmp/demo/java22/Version.java
@@ -1,6 +1,7 @@
 package net.jmp.demo.java22;
 
 /*
+ * (#)Version.java  0.7.0   08/17/2024
  * (#)Version.java  0.6.0   08/15/2024
  * (#)Version.java  0.5.0   08/10/2024
  * (#)Version.java  0.4.0   08/09/2024
@@ -9,7 +10,7 @@ package net.jmp.demo.java22;
  * (#)Version.java  0.1.0   08/02/2024
  *
  * @author    Jonathan Parker
- * @version   0.6.0
+ * @version   0.7.0
  * @since     0.1.0
  *
  * MIT License
@@ -40,7 +41,7 @@ package net.jmp.demo.java22;
  */
 final class Version {
     /** The version. */
-    static final String VERSION_STRING = "0.6.0";
+    static final String VERSION_STRING = "0.7.0";
 
     /**
      * The default constructor.
